9eac53a025d805bc6a05fad811b29371fbdce9a9
Mohammed Ibrahim Mahmoud <mohammedjrt@gmail.com>
2025-01-12 15:10:02 +0300

    chores(important): changed the main url to the dev

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -3,14 +3,13 @@ import { Observable, of } from 'rxjs';
 import { Project } from 'src/Interfaces/project';
 import {HttpClient} from '@angular/common/http'
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-  baseUrl = 'https://www.myprojectplatform.com/_functions-dev'
-  constructor(private http:HttpClient) { }
+  baseUrl = 'https://innovationladders.wixsite.com/website-6/_functions-dev';
+  constructor(private http: HttpClient) {}
 
-
-  GetProjects():Observable<Project[]>{
+  GetProjects(): Observable<Project[]> {
     return this.http.get<Project[]>(`${this.baseUrl}/projects`);
   }
 }
